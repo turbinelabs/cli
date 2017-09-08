e0ea4df03d0c8d469b8b5fe389643af59d052f27
Glen D Sanford <9len@users.noreply.github.com>
2017-09-08 23:27:33 +0000

    only do code coverage reporting in master (#3813)
    by popular meh-mand:
    
    https://turbinelabs.slack.com/archives/C0DQ6BS2K/p1504900542000254
    
    This cuts 1-2 minutes from the golang branch of the build. We still do a full coverage run for master builds, which are less time critical.
    
    fbshipit-source-id: 06758f1

--- code_coverage.sh
+++ code_coverage.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "Testing with code coverage"
+
 if [ -z $GO_TEST_TIMEOUT ]; then
   GO_TEST_TIMEOUT="4s"
 fi
@@ -23,18 +25,30 @@ touch $COVERAGE_FILE
 rm -rf $TMP_FILE
 
 for PKG in $@; do
-    touch $TMP_FILE
-    go test $GO_TEST_RUNNER \
-      -timeout $GO_TEST_TIMEOUT \
-      -covermode=count \
-      -coverprofile=$TMP_FILE \
-      $PKG || ERROR="$ERROR $PKG"
+  touch $TMP_FILE
+  go test $GO_TEST_RUNNER \
+    -timeout $GO_TEST_TIMEOUT \
+    -covermode=count \
+    -coverprofile=$TMP_FILE \
+    $PKG || ERROR="$ERROR $PKG"
 
-    cat $TMP_FILE | grep -v "/mock_" >> $COVERAGE_FILE
-    rm -rf $TMP_FILE
+  cat $TMP_FILE | grep -v "/mock_" >> $COVERAGE_FILE
+  rm -rf $TMP_FILE
 done
 
-if [ ! -z "$ERROR" ]
-then
-    die "Encountered error for one or more packages: $ERROR"
+if [ -n "$ERROR" ]; then
+  die "Encountered error for one or more packages: $ERROR"
+fi
+
+if [ -z "$TBN_COVERAGE_UPLOAD" ]; then
+  echo "skipping upload"
+  exit 0
+fi
+
+echo "TBN_COVERAGE_UPLOAD = $TBN_COVERAGE_UPLOAD"
+
+if curl --silent --fail https://codecov.io/bash >codecov.sh; then
+  bash <codecov.sh
+else
+  echo skipping codecov -- script download error
 fi
